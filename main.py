012e67a684ac5db9eee94baf84a8650a09f7a519
PhilemonVanDussen <vandussenph709@northwested.org>
2024-10-04 13:23:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# new_message = ""
+
+# user_message = input("Enter your secret message: ").lower()
+# # print(user_message)
+# key = int(input("Enter a key: "))
+
+# for character in user_message:
+#     if character in alphabet:
+#         position = alphabet.find(character)
+#         new_position = (position + key) % 26
+#         new_character = alphabet[new_position]
+#         new_message +=  new_character
+# else:
+#     new_message += character 
+# print("Your new message is " + new_message)
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 new_message = ""
 
-user_message = input("Enter your secret message: ")
+user_message = input("Enter your secret message: ").lower()
 # print(user_message)
 key = int(input("Enter a key: "))
 
 for character in user_message:
     if character in alphabet:
         position = alphabet.find(character)
-        new_position = (position + key) % 26
+        new_position = (position - key) % 26
         new_character = alphabet[new_position]
-        new_message += new_message + new_character
-
+        new_message +=  new_character
+else:
+    new_message += character 
 print("Your new message is " + new_message)
